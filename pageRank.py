0c1b8498c4840a0f09113db7bd0028a5b33accf8
Uriel Robles <uriel_r@outlook.com>
2019-03-15 12:02:16 -0600

    pagerank success

--- pageRank.py
+++ pageRank.py
@@ -2,10 +2,11 @@ def pageRank(matriz):
     from numpy.linalg import matrix_power
     import numpy as np
     from functools import reduce
+    from math import fabs
 
-    pesos = [1/(len(matriz)) for pagina in range(len(matriz))]
+    pesos_iniciales = [1/(len(matriz)) for pagina in range(len(matriz))]
 
-    def sistema_dinamico(mat, pesos, exponente=2):
+    def sistema_dinamico(mat, pesos, exponente=1):
         array = np.array(mat)
         mat = matrix_power(mat, exponente).tolist()
         
@@ -20,6 +21,22 @@ def pageRank(matriz):
             pesos_nuevos.append(reduce(lambda x, y: x + y, matriz_resultado[i]))
         return pesos_nuevos
 
-    pesos = sistema_dinamico(matriz, pesos, 8)
+    exponente_de_la_matriz = 1
+    def checkUmbral(pesos_inicio, pesos_finales, exp=1):
+        success = 0
+        for valor in range(len(pesos_iniciales)):
+            umbral = fabs(pesos_finales[valor] - pesos_inicio[valor])
+            if  umbral >= 0.0 and umbral <= 0.01:
+                success += 1
 
+        pesos_ini = sistema_dinamico(matriz, pesos_iniciales, exp)
+        if success != len(pesos_finales):
+            exp += 1
+            pesos_finales = sistema_dinamico(matriz, pesos_iniciales, exp)
+            return checkUmbral(pesos_ini, pesos_finales, exp)
+        return pesos_ini
+
+
+    pesos_finales = sistema_dinamico(matriz, pesos_iniciales)
+    pesos = checkUmbral(pesos_iniciales, pesos_finales, exponente_de_la_matriz)
     return pesos
